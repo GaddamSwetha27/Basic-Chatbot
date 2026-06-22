a956604ad99c034164209784fea9d936ccdd48e1
GaddamSwetha27 <swethareddygaddam27@gmail.com>
2026-06-22 10:59:52 +0530

    Added chatbot project

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+def chatbot():
+    print("Bot: Hello! Type 'bye' to exit.")
+
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello" or user == "hi":
+            print("Bot: Hi!")
+
+        elif user == "how are you":
+            print("Bot: I'm fine, thanks!")
+
+        elif user == "bye":
+            print("Bot: Goodbye!")
+            break
+
+        else:
+            print("Bot: Sorry, I don't understand.")
+
+chatbot()
